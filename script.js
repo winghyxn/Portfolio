d44e3954afc84c96aa260c004c66423df5db1fa8
kohweien <e1157461@u.nus.edu>
2024-05-24 01:03:57 +0800

    24/5 commit test

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let button = document.getElementById("trigger");
+    button.addEventListener("click", updateCounter);
+});
+
+function updateCounter() {
+    let counterElem = document.getElementById("counter")
+    let num = parseInt(counterEle.)
+}
