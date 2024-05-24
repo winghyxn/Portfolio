027fa91d731284887fee543fab12cb23d6509de2
kohweien <e1157461@u.nus.edu>
2024-05-24 20:43:44 +0800

    24/5 commit

---
+++ bumbledore/src/App.js
@@ -0,0 +1,14 @@
+import Login from "./Login.js"
+import './App.css';
+
+function App() {
+  return (
+    <section>
+      <Login>
+      </Login>
+    </section>
+  );
+}
+
+export default App;
+
---
+++ bumbledore/src/Login.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import './Login.css';
+
+export default function Login() {
+    const [inputs, setInputs] = useState({});
+    //const history = useHistory();
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name] : value}));
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("submitted!")
+        //navigate("www.google.com");
+    }
+
+    return (
+        <section>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+            <label>Email:  
+                <input 
+                type = "email" 
+                name = "email" 
+                value = {inputs.email || ""}
+                onChange = {handleChange}
+                required />
+            </label>
+            <br></br>
+            <label>Password:  
+                <input 
+                type = "password" 
+                name = "password" 
+                value = {inputs.password || ""}
+                onChange = {handleChange}
+                required />
+            </label>
+            <br></br>
+            <input type="submit" id="submit-button"></input>
+        </form>
+        </section>
+    )
+}
