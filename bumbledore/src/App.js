62d53a485bb0beec28c00bee69c0960219f54224
kohweien <e1157461@u.nus.edu>
2024-05-25 10:39:48 +0800

    halp

--- bumbledore/src/App.js
+++ bumbledore/src/App.js
@@ -1,11 +1,19 @@
-import Login from "./Login.js"
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Login from "./Login.js";
+import Home from "./Home.js";
 import './App.css';
 
 function App() {
   return (
     <section>
-      <Login>
-      </Login>
+      <Router>
+        <Switch>
+          <Route path = "/login" component = {Login} />
+          <PrivateRoute path = "/home" component = {Home} />
+          <Redirect from = "/" to = "/login" />
+        </Switch>
+      </Router>
     </section>
   );
 }
