e1f9e1067c23c4ea701c31e161d32350c609062f
kohweien <e1157461@u.nus.edu>
2024-06-01 21:29:54 +0800

    idk what changed

--- bumbledore/server.js
+++ bumbledore/server.js
@@ -182,3 +182,63 @@ app.listen(port, () => {
 });
 
 */
+
+/* 
+
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string
+const uri = "mongodb+srv://kweyne:tfaoAz9bCAuXWwpD@orbital.fmsrize.mongodb.net/?retryWrites=true&w=majority&appName=orbital";
+
+const client = new MongoClient(uri);
+                      
+async function run() {
+    try {
+        // Connect to the Atlas cluster
+         await client.connect();
+
+         // Get the database and collection on which to run the operation
+         const db = client.db("sample_mflix");
+         const col = db.collection("people");
+
+         // Create new documents                                                                                                                                         
+         const peopleDocuments = [
+           {
+             "name": { "first": "Alan", "last": "Turing" },
+             "birth": new Date(1912, 5, 23), // May 23, 1912                                                                                                                                 
+             "death": new Date(1954, 5, 7),  // May 7, 1954                                                                                                                                  
+             "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+             "views": 1250000
+           },
+           {
+             "name": { "first": "Grace", "last": "Hopper" },
+             "birth": new Date(1906, 12, 9), // Dec 9, 1906                                                                                                                                 
+             "death": new Date(1992, 1, 1),  // Jan 1, 1992                                                                                                                                  
+             "contribs": [ "Mark I", "UNIVAC", "COBOL" ],
+             "views": 3860000
+           }
+         ]
+
+         // Insert the documents into the specified collection        
+         const p = await col.insertMany(peopleDocuments);
+
+         // Find the document
+         const filter = { "name.last": "Turing" };
+         const document = await col.findOne(filter);
+
+         // Print results
+         console.log("Document found:\n" + JSON.stringify(document));
+
+        } catch (err) {
+         console.log(err.stack);
+     }
+ 
+     finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+*/
