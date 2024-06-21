bb260a890faf6106bec14ff35d76cd992e0682be
winghyxn <163494488+winghyxn@users.noreply.github.com>
2024-06-22 00:35:51 +0800

    Update Map.js

--- bumbledore/src/pages/Map.js
+++ bumbledore/src/pages/Map.js
@@ -1,5 +1,20 @@
+import Sidebar from "../components/sidebar.js";
+import './Home.css';
+
 export default function Map() {
     return (
-        <h2>my applications posts</h2>
+        <div className = "grid-container">
+            <div className = "sidebar">
+                <Sidebar></Sidebar>
+            </div>
+            <div className = "header">
+                <h1>My applications/posts</h1>
+            </div>
+            <div className = "main-page">
+                <div className = "content-box">
+                    <p className = "content-text">wahwah </p>
+                </div>
+            </div>
+        </div>
     );
 }
