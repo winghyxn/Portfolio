b80c4fa59309d8a4a55cc227602bfaa6d0398ea0
kohweien <e1157461@u.nus.edu>
2024-06-27 21:18:37 +0800

    added basic css of messages

--- bumbledore/src/pages/Messages.js
+++ bumbledore/src/pages/Messages.js
@@ -1,5 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import useToken from "../components/useToken.js";
+import styles from './Messages.module.css';
+
 export default function Messages() {
     return (
-        <h2>messages</h2>
+        <div className={styles.gridContainer}>
+            <div className={styles.sidebar}>
+                <a href = "/home">Back</a>
+                <p>messagesssssssssssssssssss</p>
+            </div>
+            <div className={styles.header}>
+                <p>username</p>
+            </div>
+            <div className={styles.mainPage}>
+                <p>texts</p>
+            </div>
+            <div className={styles.textBar}>
+                <form className={styles.textBarForm}>
+                    <textarea className={styles.textBarFormInputs} />
+                    <button className={styles.textBarFormIputs} type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
     );
 }
