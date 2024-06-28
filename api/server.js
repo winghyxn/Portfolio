b0add14aaf9275e04c9b931e5c7c48f05b61a66f
winghyxn <163494488+winghyxn@users.noreply.github.com>
2024-06-28 23:03:47 +0800

    Update server.js

--- api/server.js
+++ api/server.js
@@ -153,14 +153,14 @@ app.post('/my-profile', async (req, res) => {
 });
 
 app.get('/my-profile', async (req, res) => {
-  const token = req.query.token;
+  const username = req.query.username;
   
   try {
     await client.connect();
     const db = client.db("bumbledore");
-    const posts = db.collection("userProfileInfo");
+    const profiles = db.collection("userProfileInfo");
 
-    const profile = await posts.find({ email: token }).toArray();
+    const profile = await profiles.findOne({ username: username });
     res.status(200).json(profile);
   } catch (error) {
     console.error('Error fetching posts:', error);
@@ -168,7 +168,7 @@ app.get('/my-profile', async (req, res) => {
   } finally {
     await client.close();
   }
-}
+});
 
 app.get('/posts', async (req, res) => {
   try {
@@ -229,6 +229,122 @@ app.get('/user-profile', async (req, res) => {
   } catch (error) {
       console.error('Error fetching profile:', error);
       res.status(500).send('Failed to fetch profile');
+  } finally {
+      await client.close();
+  }
+});
+
+app.post('/new-chat', async (req, res) => {
+  const { username, profile } = req.body;
+
+  try {
+      await client.connect();
+      const database = client.db('bumbledore');
+      const allChats = database.collection('chats');
+
+      const selfHasChats = await allChats.findOne({ username: username });
+      const otherHasChats = await allChats.findOne({ username: profile });
+
+      if (!selfHasChats) {
+        await allChats.insertOne({
+          username: username, 
+          chats: [profile]
+        });
+      } else {
+        await allChats.updateOne(
+          { username: username },
+          { $addToSet: { chats: profile } }
+        );
+      }
+
+      if (!otherHasChats){
+        await allChats.insertOne({
+          username: profile,
+          chats: [username]
+        });
+      } else {
+        await allChats.updateOne(
+          { username: profile },
+          { $addToSet: { chats: username } }
+        );
+      }
+
+      res.status(200).send('Chats updated successfully');
+  } catch (error) {
+      console.error('Error updating chats:', error);
+      res.status(500).send('Failed to update chats');
+  } finally {
+      await client.close();
+  }
+});
+
+app.get('/chats', async (req, res) => {
+  const username = req.query.username;
+
+  try {
+      await client.connect();
+      const db = client.db("bumbledore");
+      const allChats = db.collection("chats");
+
+      const chats = await allChats.findOne({ username: username });
+      /*if (!profile) {
+          return res.status(404).send('User profile not found');
+      }*/
+      res.status(200).json(chats);
+  } catch (error) {
+      console.error('Error fetching profile:', error);
+      res.status(500).send('Failed to fetch profile');
+  } finally {
+      await client.close();
+  }
+});
+
+app.post('/messages', async (req, res) => {
+  const { sender, recipient, message } = req.body;
+
+  try {
+      await client.connect();
+      const database = client.db('bumbledore');
+      const allMessages = database.collection('messages');
+
+      const messageInfo = {
+        sender: sender,
+        recipient: recipient,
+        message: message,
+        createdAt: new Date(),
+      };
+
+      await allMessages.insertOne(messageInfo);
+
+      res.status(200).send({messageInfo});
+  } catch (error) {
+      console.error('Error sending message:', error);
+      res.status(500).send('Failed to send message');
+  } finally {
+      await client.close();
+  }
+});
+
+app.get('/messages', async (req, res) => {
+  const sender = req.query.sender;
+  const recipient = req.query.recipient;
+
+  try {
+      await client.connect();
+      const db = client.db("bumbledore");
+      const allMessages = db.collection("messages");
+
+      const sort = { createdAt: 1 };
+      const messages = await allMessages.find({
+        $or: [
+          { $and: [ {sender: sender}, {recipient: recipient}] },
+          { $and: [ {sender: recipient}, {recipient: sender}]}
+        ]
+      }).sort(sort).toArray(); 
+      res.status(200).json(messages);
+  } catch (error) {
+      console.error('Error fetching messages:', error);
+      res.status(500).send('Failed to fetch messages');
   } finally {
       await client.close();
   }
