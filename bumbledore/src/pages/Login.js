98fa126bc03d9c53391af6a3c9c6e0c9e1a9c1cb
kohweien <e1157461@u.nus.edu>
2024-07-04 20:33:53 +0800

    fixing login bugs

--- bumbledore/src/pages/Login.js
+++ bumbledore/src/pages/Login.js
@@ -30,6 +30,7 @@ export default function Login({ setToken }) {
         event.preventDefault();
         setError(null); // Clear previous error
         const response = await Authentication(inputs);
+        setToken(response);
 
         try {
             /*const response = await fetch("http://localhost:3000/login", {  //`${API_URL}/login`, {//fetch('', { https://bumbledore.vercel.app/login 
@@ -45,7 +46,7 @@ export default function Login({ setToken }) {
                 navigate('/home');
 
             } else {
-            const errorMessage = await response.text();
+            //const errorMessage = await response.text();
             setError(errorMessage);
         }
 
