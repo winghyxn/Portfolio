d4e95a540c980a14a7c332b4931df0b0a350993a
winghyxn <163494488+winghyxn@users.noreply.github.com>
2024-07-07 16:36:28 +0800

    Update Home.js

--- bumbledore/src/pages/Home.js
+++ bumbledore/src/pages/Home.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
-import Sidebar from '../components/sidebar'; // Adjust the path as needed
+import Sidebar from '../components/sidebar.js'; // Adjust the path as needed
 import useToken from "../components/useToken.js";
 import './Home.css';
 import styles from '../components/post.module.css';
+import loaderStyles from '../components/loader.module.css';
 
 export default function Home() {
     const [posts, setPosts] = useState([]);
     const { token } = useToken();
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
 
     // Assuming token contains the username directly
@@ -17,8 +19,9 @@ export default function Home() {
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const response = await axios.get('http://localhost:8080/posts');
+                const response = await axios.get('https://bumbledore-server.vercel.app/posts');
                 setPosts(response.data);
+                setLoading(false);
             } catch (error) {
                 console.error('Failed to fetch posts:', error);
             }
@@ -29,15 +32,15 @@ export default function Home() {
 
     const handleMessageRequest = async (e) => {
         e.preventDefault();
-        const postUsername = e.target.dataset.username;
+        const profile = e.target.dataset.profile;
 
         const userData = {
-            username: postUsername,
-            profile: postUsername
+            username: username,
+            profile: profile
         };
 
         try {
-            const response = await axios.post('http://localhost:8080/new-chat', userData);
+            const response = await axios.post('https://bumbledore-server.vercel.app/new-chat', userData);
             if (response.status === 200) {
                 console.log('Chat created successfully:', response.data);
                 navigate('/messages');
@@ -57,34 +60,41 @@ export default function Home() {
             <div className="header">
                 <h1>All Posts</h1>
             </div>
-            <div className="main-page">
-                {posts.length > 0 ? (
-                    posts.map((post) => (
-                        <div key={post._id} className={styles.post}>
-                            <h3 className={styles.header}>
-                                Posted by: <Link className={styles.text} to={`/profile/${post.username}`}>{post.username}</Link>
-                            </h3>
-                            <p className={styles.text}>Course Code: {post.courseCode}</p>
-                            <p className={styles.text}>Type of Request: {post.typeOfRequest}</p>
-                            <p className={styles.text}>Description: {post.description}</p>
-                            {post.pay && <p className={styles.text}>Pay: {post.pay}</p>}
-                            {post.numGroupmates && <p className={styles.text}>Number of Groupmates Needed: {post.numGroupmates}</p>}
-                            <p className={styles.text}>Created At: {new Date(post.createdAt).toLocaleString()}</p>
-                            {post.username !== username && (
-                                <button 
-                                    onClick={handleMessageRequest} 
-                                    type="button"
-                                    data-username={post.username}
-                                    data-profile={post.username}>
-                                        Message
-                                </button>
-                            )}
-                        </div>
-                    ))
+            {loading ? (
+                <div className="main-page">
+                    <div className={loaderStyles.loader}></div>
+                    <p>loading...</p>
+                </div>
                 ) : (
-                    <p>No posts available</p>
+                    <div className="main-page">
+                        {posts.length > 0 ? (
+                            posts.map((post) => (
+                                <div key={post._id} className={styles.post}>
+                                    <h3 className={styles.header}>
+                                        Posted by: <Link className={styles.text} to={`/profile/${post.username}`}>{post.username}</Link>
+                                    </h3>
+                                    <p className={styles.text}>Course Code: {post.courseCode}</p>
+                                    <p className={styles.text}>Type of Request: {post.typeOfRequest}</p>
+                                    <p className={styles.text}>Description: {post.description}</p>
+                                    {post.pay && <p className={styles.text}>Pay: {post.pay}</p>}
+                                    {post.numGroupmates && <p className={styles.text}>Number of Groupmates Needed: {post.numGroupmates}</p>}
+                                    <p className={styles.text}>Created At: {new Date(post.createdAt).toLocaleString()}</p>
+                                    {post.username !== username && (
+                                        <button 
+                                            onClick={handleMessageRequest} 
+                                            type="button"
+                                            data-username={username}
+                                            data-profile={post.username}>
+                                                Message
+                                        </button>
+                                    )}
+                                </div>
+                            ))
+                        ) : (
+                            <p>No posts available</p>
+                        )}
+                    </div>
                 )}
-            </div>
         </div>
     );
 }
