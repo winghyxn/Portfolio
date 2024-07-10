b9d00965285c563395d95420b49f01836621916f
winghyxn <163494488+winghyxn@users.noreply.github.com>
2024-07-11 00:22:03 +0800

    Update Home.js

--- bumbledore/src/pages/Home.js
+++ bumbledore/src/pages/Home.js
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
-import Sidebar from '../components/sidebar.js'; // Adjust the path as needed
+import Sidebar from '../components/sidebar'; // Adjust the path as needed
 import useToken from "../components/useToken.js";
 import './Home.css';
 import styles from '../components/post.module.css';
-import loaderStyles from '../components/loader.module.css';
 
 export default function Home() {
     const [posts, setPosts] = useState([]);
     const { token } = useToken();
-    const [loading, setLoading] = useState(true);
-    const [appliedPosts, setAppliedPosts] = useState({});
     const navigate = useNavigate();
 
     // Assuming token contains the username directly
@@ -20,10 +17,8 @@ export default function Home() {
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const response = await axios.get('https://api-wing-s-projects.vercel.app/posts');
-                const openPosts = response.data.filter(post => post.status === 'open');
-                setPosts(openPosts);
-                setLoading(false);
+                const response = await axios.get('mongodb+srv://kweyne:tfaoAz9bCAuXWwpD@orbital.fmsrize.mongodb.net/?retryWrites=true&w=majority&appName=orbital/posts');
+                setPosts(response.data);
             } catch (error) {
                 console.error('Failed to fetch posts:', error);
             }
@@ -34,17 +29,15 @@ export default function Home() {
 
     const handleMessageRequest = async (e) => {
         e.preventDefault();
-        const profile = e.target.dataset.profile;
-        const postID = e.target.dataset.postid;
+        const postUsername = e.target.dataset.username;
 
         const userData = {
-            username: username,
-            profile: profile,
-            postID: postID
+            username: postUsername,
+            profile: postUsername
         };
 
         try {
-            const response = await axios.post('https://api-wing-s-projects.vercel.app/new-chat', userData);
+            const response = await axios.post('mongodb+srv://kweyne:tfaoAz9bCAuXWwpD@orbital.fmsrize.mongodb.net/?retryWrites=true&w=majority&appName=orbital/new-chat', userData);
             if (response.status === 200) {
                 console.log('Chat created successfully:', response.data);
                 navigate('/messages');
@@ -56,46 +49,6 @@ export default function Home() {
         }
     };
 
-    const handleApplyRequest = async (postId, poster) => {
-        try {
-            // Apply to the post
-            const response = await axios.patch(`https://api-wing-s-projects.vercel.app/posts/${postId}/apply`, { username });
-            if (response.status === 200) {
-                console.log('Applied successfully');
-                setAppliedPosts(prevState => ({ ...prevState, [postId]: true }));
-
-                // Create a chat and send an automatic message
-                const chatResponse = await axios.post('https://api-wing-s-projects.vercel.app/new-chat', {
-                    username: username,
-                    profile: poster,
-                    postID: postId
-                });
-
-                if (chatResponse.status === 200) {
-                    const messageResponse = await axios.post('https://api-wing-s-projects.vercel.app/messages', {
-                        sender: username,
-                        recipient: poster,
-                        postID: postId,
-                        message: "Hi! I have applied for your post!"
-                    });
-
-                    if (messageResponse.status === 200) {
-                        console.log('Message sent successfully');
-                        navigate('/messages');
-                    } else {
-                        console.error('Failed to send message: Status code:', messageResponse.status);
-                    }
-                } else {
-                    console.error('Failed to create chat: Status code:', chatResponse.status);
-                }
-            } else {
-                console.error('Failed to apply:', response.status);
-            }
-        } catch (error) {
-            console.error('Failed to apply:', error);
-        }
-    };
-
     return (
         <div className="grid-container">
             <div className="sidebar">
@@ -104,55 +57,34 @@ export default function Home() {
             <div className="header">
                 <h1>All Posts</h1>
             </div>
-            {loading ? (
-                <div className="main-page">
-                    <div className={loaderStyles.loader}></div>
-                    <p>Loading...</p>
-                </div>
-            ) : (
-                <div className="main-page">
-                    {posts.length > 0 ? (
-                        posts.map((post) => (
-                            <div key={post._id} className={styles.post}>
-                                <h3 className={styles.header}>{post._id}</h3>
-                                <h3 className={styles.header}>
-                                    Posted by: <Link className={styles.text} to={`/profile/${post.username}`}>{post.username}</Link>
-                                </h3>
-                                <p className={styles.text}>Course Code: {post.courseCode}</p>
-                                <p className={styles.text}>Type of Request: {post.typeOfRequest}</p>
-                                <p className={styles.text}>Description: {post.description}</p>
-                                {post.pay && <p className={styles.text}>Pay: {post.pay}</p>}
-                                {post.numGroupmates && <p className={styles.text}>Number of Groupmates Needed: {post.numGroupmates}</p>}
-                                <p className={styles.text}>Created At: {new Date(post.createdAt).toLocaleString()}</p>
-                                {post.username !== username && (
-                                    <>
-                                        <button 
-                                            onClick={handleMessageRequest} 
-                                            type="button"
-                                            data-username={username}
-                                            data-profile={post.username}
-                                            data-postid={post._id.toString()}>
-                                                Message
-                                        </button>
-                                        {appliedPosts[post._id] ? (
-                                            <p>Post applied successfully</p>
-                                        ) : (
-                                            <button
-                                                onClick={() => handleApplyRequest(post._id, post.username)}
-                                                type="button"
-                                            >
-                                                Apply
-                                            </button>
-                                        )}
-                                    </>
-                                )}
-                            </div>
-                        ))
-                    ) : (
-                        <p>No posts available</p>
-                    )}
-                </div>
-            )}
+            <div className="main-page">
+                {posts.length > 0 ? (
+                    posts.map((post) => (
+                        <div key={post._id} className={styles.post}>
+                            <h3 className={styles.header}>
+                                Posted by: <Link className={styles.text} to={`/profile/${post.username}`}>{post.username}</Link>
+                            </h3>
+                            <p className={styles.text}>Course Code: {post.courseCode}</p>
+                            <p className={styles.text}>Type of Request: {post.typeOfRequest}</p>
+                            <p className={styles.text}>Description: {post.description}</p>
+                            {post.pay && <p className={styles.text}>Pay: {post.pay}</p>}
+                            {post.numGroupmates && <p className={styles.text}>Number of Groupmates Needed: {post.numGroupmates}</p>}
+                            <p className={styles.text}>Created At: {new Date(post.createdAt).toLocaleString()}</p>
+                            {post.username !== username && (
+                                <button 
+                                    onClick={handleMessageRequest} 
+                                    type="button"
+                                    data-username={post.username}
+                                    data-profile={post.username}>
+                                        Message
+                                </button>
+                            )}
+                        </div>
+                    ))
+                ) : (
+                    <p>No posts available</p>
+                )}
+            </div>
         </div>
     );
 }
