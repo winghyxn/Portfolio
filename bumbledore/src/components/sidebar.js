7f1827b3817a521cf97c7dc5fad32419b5c5a867
winghyxn <163494488+winghyxn@users.noreply.github.com>
2024-07-10 17:10:37 +0800

    finish My Applications and My posts

--- bumbledore/src/components/sidebar.js
+++ bumbledore/src/components/sidebar.js
@@ -11,9 +11,11 @@ export default function Sidebar() {
                 <br></br>
                 <a href = "/messages">Direct Messages</a>
                 <br></br>
-                <a href = "/my-applications-posts">My applications/posts</a>
+                <a href = "/my-posts">My Posts</a>
                 <br></br>
-                <a href = "/profile">My profile</a>
+                <a href = "/my-applications">My Applications</a>
+                <br></br>
+                <a href = "/profile">My Profile</a>
             </div>
         </div>
     );
